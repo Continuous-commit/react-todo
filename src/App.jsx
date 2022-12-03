03d91ccf516e7741a4cccf5f19df86b74109aec4
Continuous-commit <fuki217@yahoo.co.jp>
2022-12-03 13:19:53 +0900

    feat: タスク表示とチェックボックス

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 export const App = () => {
   // 状態を変化させる際useStateを使用
+  const [todoList, setTodoList] = useState([]);
   const [todoText, setTodotext] = useState({ id: 0, body: "", isCompleted: false });
-  const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [editFlag, setEditFlag] = useState(true);
-  const [editedTodo, setEditedTodo] = useState({ id: 0, body: "", isCompleted: false })
+  const [editedTodo, setEditedTodo] = useState({ id: 0, body: "", isCompleted: false });
 
-  const onChangeTodoText = (e) => setTodotext(e.target.value);
+  const allTaskCount = () => todoList.length
 
-  const onChangeTodoEditText = (event, index) => {
-    const todos = [...incompleteTodos];
-    todos[index].data = event.target.value;
-    setIncompleteTodos(todos);
-  };
+  const inCompletedCount = () => todoList.filter(todoText => !(todoText.isCompleted)).length
+
+  const completedCount = () => todoList.filter(todoText => todoText.isCompleted).length
+
+  const handleNewTodo = (e) => setTodotext({ ...todoText, body: e.target.value });
+
+  // const onChangeTodoEditText = (event, index) => {
+  //   const todos = [...incompleteTodos];
+  //   todos[index].data = event.target.value;
+  //   setIncompleteTodos(todos);
+  // };
 
   const onClickAdd = (e) => {
     e.preventDefault()
-    if (todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText ];
-    setIncompleteTodos(newTodos);
+    if (todoText.body === "") return;
+    setTodoList([...todoList, todoText]);
     setTodotext({id: todoText.id + 1, body: "", isCompleted: false});
   };
 
+  const onClickComplete = (id) => {
+    const completeList = todoList.map((todo) => {
+      if(todo.id === id) todo.isCompleted = !todo.isCompleted
+      return todo
+    })
+    setTodoList(completeList);
+  };
+
   const onClickDelete = (id) => {
-    const newTodos = [...incompleteTodos];
+    const newTodos = [...todoList];
     newTodos.splice(id, 1);
-    setIncompleteTodos(newTodos);
+    setTodoList(newTodos);
   };
 
   const onClickEdit = (id) => {
@@ -42,48 +54,55 @@ export const App = () => {
     setEditedTodo(handleEdit);
   };
 
-  const saveTask = () => {
+  const saveTask = (id) => {
     if (editedTodo === "") return
-    const newTodoList = incompleteTodos.map(todo => {
+    const newTodoList = todoList.map((todo) => {
       if(todo.id === editedTodo.id) todo.body = editedTodo.body
       return todo
     })
-    setIncompleteTodos(newTodoList)
-    setEditFlag(true)
+    setTodoList(newTodoList);
+    setEditFlag(true);
   }
   return (
-    <>
+    <div className="wrapper">
       <div className="input-area">
-        <input placeholder="TODOを入力" value={todoText.body} onChange={onChangeTodoText} />
+        <input placeholder="TODOを入力" value={todoText.body} onChange={handleNewTodo} />
         <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div>
+        <ul>
+          <li>全てのタスク: {allTaskCount()}</li>
+          <li>完了済: {completedCount()}</li>
+          <li>未完了: {inCompletedCount()}</li>
+        </ul>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
-            {
-              if (editFlag) {
-                return (
-                  <div key={todo} className="list-row">
-                    <li>{todo}</li>
-                    <button onClick={() => {onClickEdit(todo.id)}}>編集</button>
-                    {/* ループしないようにアロー関数で！ */}
-                    <button onClick={() => onClickDelete(todo.id)}>削除</button>
-                  </div>
-                )
-              } else {
-                return (
-                  // ループする際親要素にkeyを設定(差分をkeyを目印に抽出するため)
-                  <div key={todo}>
-                    <input value={todo.body} onChange={handleEditTodo} />
-                    <button onClick={saveTask}>保存</button>
-                  </div>
-                );
-              }
+          {todoList.map((todo) => {
+            if (editFlag) {
+              return (
+                <div key={todo} className="list-row">
+                  <input type="checkbox" onClick={() => {onClickComplete(todo.id)}} />
+                  <li>{todo.body}</li>
+                  <button onClick={() => {onClickEdit(todo.id)}}>編集</button>
+                  {/* ループしないようにアロー関数で！ */}
+                  <button onClick={() => onClickDelete(todo.id)}>削除</button>
+                </div>
+              )
+            } else {
+              return (
+                // ループする際親要素にkeyを設定(差分をkeyを目印に抽出するため)
+                <div key={todo}>
+                  <input type="checkbox" onClick={() => {onClickComplete(todo.id)}} />
+                  <input onChange={handleEditTodo} />
+                  <button onClick={() => saveTask(todo.id)}>保存</button>
+                </div>
+              );
             }
           })}
         </ul>
       </div>
-    </>
+    </div>
   );
 };
